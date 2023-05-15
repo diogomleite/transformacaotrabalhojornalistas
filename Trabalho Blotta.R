d2ce65ae3b7b392aa128d047a7923b7f42431d92
Diogo Leite <121272480+diogomleite@users.noreply.github.com>
2023-05-15 01:09:51 -0300

    Add files via upload

---
+++ Trabalho Blotta.R
@@ -0,0 +1,86 @@
+#Vínculos precários
+
+reg_periodo1 <- lm(ano_crescimento ~ fracao_crescimento, data = precarios_prevalencia)
+summary(reg_periodo1)
+reg_periodo2 <- lm(ano_atual ~ fracao_atual, data = precarios_prevalencia)
+summary(reg_periodo2)
+
+#Carga horária
+
+reg <- lm(ano ~ quantidade_horas_contratadas, data = horas)
+summary(reg)
+horas1 <- horas[-(18:28),]
+reg_aumento <- lm(ano ~ quantidade_horas_contratadas, data = horas1)
+summary(reg_aumento)
+horas2 <- horas[-(1:16),]
+reg_queda <- lm(ano ~ quantidade_horas_contratadas, data = horas2)
+summary(reg_queda)
+
+#Tempo no emprego
+
+reg <- lm(ano ~ tempo_emprego, data = tempo)
+summary(reg)
+tempo1 <- tempo[-(1:16),]
+reg_periodo1 <- lm(ano ~ tempo_emprego, data = tempo1)
+summary(reg_periodo1)
+tempo2 <- tempo[-(18:28),]
+reg_periodo1 <- lm(ano ~ tempo_emprego, data = tempo2)
+summary(reg_periodo1)
+
+#idade
+
+reg <- lm(ano ~ idade, data = idade)
+summary(reg)
+idade1 <- idade[-(18:28),]
+reg_queda <- lm(ano ~ idade, data = idade1)
+summary(reg_queda)
+idade2 <- idade[-(1:16),]
+reg_aumento <- lm(ano ~ idade, data = idade2)
+summary(reg_aumento)
+
+#número de profissionais
+
+regpre <- lm(ano_pre2002 ~ total_pre2002, data = profissionais)
+summary(regpre)
+regpos <- lm(ano_pos2002 ~ total_pos2002, data = profissionais)
+summary(regpos)
+reg_queda <- lm(ano_queda ~ total_queda, data = profissionais)
+summary(reg_queda)
+
+#Salário
+reg <- lm(ano ~ remuneracao, data = salário)
+summary(reg)
+salario1 <- salário[-(1:13),]
+reg_queda <- lm(ano ~ remuneracao, data = salario1)
+summary(reg_queda)
+
+#Assessores
+
+reg <- lm(ano ~ prevalencia, data = assessores)
+summary(reg)
+
+#Gráficos
+
+plot(assessores$ano, assessores$prevalencia, type = 'l', main = 'Prevalência de assessores', ylab = 'Fração de assessores', xlab = 'Ano')
+plot(assessores$ano, assessores$total, type = 'l', main = 'Número total de assessores', ylab = 'Número de assessores', xlab = 'Ano')
+
+par(mfrow = c(2, 1))
+plot(profissionais$ano_pre2002, profissionais$total_pre2002, type = 'l', main = 'Número total de profissionais (1985-2002)', ylab = 'Número de profissionais', xlab = 'Ano')
+plot(profissionais$ano_pos2002, profissionais$ano_pos2002, type = 'l', main = 'Número total de profissionais (2002-2020)', ylab = 'Número de profissionais', xlab = 'Ano')
+
+par(mfrow = c(1, 1))
+plot(idade$ano, idade$idade, type = 'l', main = 'Idade média dos profissionais', ylab = 'Idade média (anos)', xlab = 'Ano')
+plot(tempo$ano, tempo$tempo_emprego, type = 'l', main = 'Tempo médio de permanência no emprego', ylab = 'Tempo médio (meses)', xlab = 'Ano')
+plot(salário$ano, salário$remuneracao, type = 'l', main = 'Salário médio', ylab = 'Salário médio (nº de salários mínimos)', xlab = 'Ano')
+plot(horas$ano, horas$quantidade_horas_contratadas, type = 'l', main = 'Carga horária contratada', ylab = 'Carga horária média (horas)', xlab = 'Ano')
+
+par(mfrow = c(2, 1))
+plot(precarios$ano_pre2002, precarios$total_pre2002, type = 'l', main = 'Número de vínculos de trabalho precários (1985 - 2002)', ylab = 'Número de vínculos de trabalho precários', xlab = 'Ano')
+plot(precarios$ano_pos2002, precarios$total_pos2002, type = 'l', main = 'Número de vínculos de trabalho precários (2003 - 2020)', ylab = 'Número de vínculos de trabalho precários', xlab = 'Ano')
+par(mfrow = c(1, 1))
+plot(precarios_prevalencia$ano, precarios_prevalencia$fracao, type = 'l', main = 'Prevalência de vínculos precários', ylab = 'Fração de vínculos precários', xlab = 'Ano')
+
+#Correlação Carga horária vs. Total de profissionais
+horas <- horas[-(1:9),]
+profissionais <- profissionais[-(1:18),]
+cor(profissionais$total, horas$quantidade_horas_contratadas)
